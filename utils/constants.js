ed23625c5431b3e940973e0f9fb02c2876f1ca9b
F0BiYa <fobiya1982@yandex.ru>
2022-07-10 16:35:40 +0300

    new base adress

--- utils/constants.js
+++ utils/constants.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // прописать порт и адресс базы
 const {
   PORT = 3000,
-  MONGO_URL = 'mongodb://localhost:27017/moviesdb',
+  MONGO_URL = 'mongodb://localhost:27017/newMoviesdb',
 } = process.env;
 
 module.exports = {
