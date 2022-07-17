6b83fa8e6275e5416e953b2f053918bd5b9fe712
F0BiYa <fobiya1982@yandex.ru>
2022-07-17 17:54:27 +0300

    back fix mongodb

--- utils/constants.js
+++ utils/constants.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // прописать порт и адресс базы
 const {
   PORT = 3000,
-  MONGO_URL = 'mongodb://localhost:27017/newMoviesdb',
+  MONGO_URL = 'mongodb://localhost:27017/newExplorerMoviesdb',
 } = process.env;
 
 module.exports = {
